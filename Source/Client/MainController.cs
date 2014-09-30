b0d40d9b1b4a533038f387f79a570267ade66832
Bulow <emilbulow@outlook.com>
2014-09-30 23:53:06 +0200

    Added Ocr.cs for communicating with server

---
+++ Source/Client/MainController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace Budgeteer
+{
+	public class MainController
+	{
+		public MainController ()
+		{
+		}
+
+		/// <summary>
+		/// Called from each platform specific UI when a picture of a receipt has been taken
+		/// </summary>
+		/// <param name="imageStream">Stream containing the picture of the receipt</param>
+		public void OnPictureTaken(Stream imageStream)
+		{
+			Ocr.BeginGetOcrRequest (imageStream);
+		}
+
+
+	}
+}
+
---
+++ Source/Client/Ocr.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace Budgeteer
+{
+	/// <summary>
+	/// Handles all communication with the server
+	/// </summary>
+	public class Ocr
+	{
+		/// <summary>
+		/// Event that fires when data has been received from the server
+		/// </summary>
+		public static event EventHandler<string> ReceivedOcrData;
+
+		/// <summary>
+		/// Begins getting the request stream for the server. This is used for uploading the image
+		/// </summary>
+		/// <param name="imageStream">Image stream.</param>
+		public static void BeginGetOcrRequest(Stream imageStream)
+		{
+			//TODO: make central data store for urls like this one
+			Uri uri = new Uri ("http://api.budgeteer/devbase.biz/performOcr.php", UriKind.Absolute);
+			HttpWebRequest request = HttpWebRequest.CreateHttp (uri);
+			request.Method = "POST";
+
+			request.BeginGetRequestStream (new AsyncCallback(EndGetOcrRequest), new Tuple<HttpWebRequest, Stream>(request, imageStream));
+		}
+
+		/// <summary>
+		/// Ends getting the request stream for the server and uploads the image
+		/// 
+		/// This function is only called by BeginGetRequestStream
+		/// </summary>
+		/// <param name="result">Result.</param>
+		static async void EndGetOcrRequest(IAsyncResult result)
+		{
+			Stream imageStream = ((Tuple<HttpWebRequest, Stream>)result.AsyncState).Item2;
+			HttpWebRequest request = ((HttpWebRequest)((Tuple<HttpWebRequest, Stream>)result.AsyncState).Item1);
+			Stream requestStream = request.EndGetRequestStream (result);
+			await imageStream.CopyToAsync (requestStream);
+
+			request.BeginGetResponse (new AsyncCallback (EndGetOcrResponse), request);
+		}
+
+		/// <summary>
+		/// Gets the response from the server
+		/// 
+		/// This function is only called by BeginGetRequestStream
+		/// </summary>
+		/// <param name="result">Result.</param>
+		static async void EndGetOcrResponse(IAsyncResult result)
+		{
+			HttpWebRequest request = (HttpWebRequest)result.AsyncState;
+			HttpWebResponse response = request.EndGetResponse (result);
+
+			Stream responseStream = response.GetResponseStream ();
+			string ocrText = await new StreamReader (responseStream).ReadToEndAsync ();
+
+			//Fire event that notifies the rest of the app that data has been received from the server
+			if (ReceivedOcrData != null)
+				ReceivedOcrData (null, ocrText);
+		}
+	}
+}
+
